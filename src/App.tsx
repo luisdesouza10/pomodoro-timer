15852a9a0fd2e80ccf06fa33ff302c794bae3fee
Luis Eduardo <luis@leaderweb.com.br>
2022-09-08 09:58:49 -0300

    global styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  );
 }
